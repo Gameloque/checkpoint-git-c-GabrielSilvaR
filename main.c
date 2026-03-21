35ad74e1a4897ebd991f52eb6736f40511e1f493
Gameloque <gabrielhenriqueff1448@gmail.com>
2026-03-21 12:43:50 -0300

    Corrigio o Erro do código

--- main.c
+++ main.c
@@ -4,8 +4,11 @@ int main(void){
     printf(" -Painel de Membros- \n");
         printf("Membros:\n");
     printf("     Gabriel Henrique Silva ");
+        printf("  -RM:");
+    printf(" 573093 \n");
 
 
 
+    printf(" Sejam bem vindos! ");
     return 0;
 }
